cc6dcb0b41cf9f71ebcfed105bbe0b22ee652a7f
Amanda-kang <60984187+Amanda-kang@users.noreply.github.com>
2020-04-09 18:16:03 +0100

    Update regional.js

--- scripts/regional.js
+++ scripts/regional.js
@@ -19,6 +19,6 @@ function topFunction() {
 }
 
 //test for touch events support and if not supported, attach .no-touch class to the HTML tag.
-if (!(":hover" in document.documentElement)) {
+if (!("ontouchstart:" in document.documentElement)) {
   document.documentElement.className += "no-touch";
   }
